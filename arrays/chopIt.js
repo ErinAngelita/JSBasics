ef051ab413d5fb38a85b543c644fba7165082a3e
erin olsen <erinolsen@erins-MacBook-Pro.local>
2018-02-06 15:26:05 -0700

    test_3

--- arrays/chopIt.js
+++ arrays/chopIt.js
@@ -1,6 +1,9 @@
 // Create a function called chopIt that takes in an array and two numbers.
 // It deletes the original 3rd index value and inserts the two numbers in it's place.
-
+function chopIt(arr, num1, num2) {
+  arr.splice(3, 1, num1, num2);
+    return arr;
+};
 var chopIt;
 
 // DO NOT DELETE
