ac0104a5b9ee4f0663e7e7388fb02a8354cf25c7
erin olsen <erinolsen@erins-MacBook-Pro.local>
2018-02-06 15:13:36 -0700

    test_1

--- arrays/addEnd.js
+++ arrays/addEnd.js
@@ -1,3 +1,9 @@
+function addEnd(userinput1, userinput2) {
+  userinput1.push(userinput2);
+  return userinput1;
+}
+
+
 // Create a function called addEnd that takes in an array and a number and
 // positions the number at the end of the array. Return the new array.
 
--- arrays/addFront.js
+++ arrays/addFront.js
@@ -1,7 +1,11 @@
 // Create a function called addFront that takes in an array and a number and
 // positions the number at the front of the array. Return the new array.
+function addFront(param1, param2) {
+  param1.unshift(param2);
+  return(param1);
+};
 
-var addFront;
+
 
 // DO NOT DELETE
 module.exports = addFront;
