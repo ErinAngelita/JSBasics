9fe9035404350ef11c514de08a8fc9d23b384177
erin olsen <erinolsen@erins-MacBook-Pro.local>
2018-02-06 16:22:41 -0700

    test 9

--- arrays/countString.js
+++ arrays/countString.js
@@ -1,8 +1,16 @@
 // Write a JavaScript function that accepts two arguments,
 // a string and a letter and the function will count the number
 // of occurrences of the specified letter within the string.
-
-var countString;
+function countString(school, o) {
+  var count = 0;
+  school = school.toLowerCase();
+  o = o.toLowerCase();
+  for(var i = 0; i < school.length; ++i) {
+    if(school[i] == o)
+    count++;
+  }
+  return count;
+};
 
 // DO NOT DELETE
 module.exports = countString;
