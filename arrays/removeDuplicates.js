06c7b9dbcdf434ba8b95707efd12e25385745bd5
Erin Olsen <erin.angelita@gmail.com>
2018-02-07 10:36:09 -0700

    test 9

--- arrays/removeDuplicates.js
+++ arrays/removeDuplicates.js
@@ -1,5 +1,13 @@
 // Write a JavaScript function removeDuplicates that removes duplicate values fom an array.
-
+function removeDuplicates(arr){
+  var newArray = []
+  for (var i = 0; i < arr.length; i++){
+    if (newArray.indexOf(arr[i]) == -1) {
+      newArray.push(arr[i])
+    }
+  }
+  return newArray
+}
 var removeDuplicates;
 
 // DO NOT DELETE
