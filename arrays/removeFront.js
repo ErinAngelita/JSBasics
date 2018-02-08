7ae0dfb4ea3f182402a51f7a5930c0e903985df9
Erin Olsen <erin.angelita@gmail.com>
2018-02-08 10:14:41 -0700

    remove front

--- arrays/removeFront.js
+++ arrays/removeFront.js
@@ -1,6 +1,9 @@
 // Create a function called removeFront that takes in an array and removes the
 // value at the front of the array. Return the new array.
-
+function removeFront(arr){
+  arr.shift();
+  return arr;
+}
 var removeFront;
 
 // DO NOT DELETE
