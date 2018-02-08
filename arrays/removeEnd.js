9cde46dcdcf32acb76fbdb99db3f539559055c4e
Erin Olsen <erin.angelita@gmail.com>
2018-02-08 10:09:03 -0700

    remove end

--- arrays/removeEnd.js
+++ arrays/removeEnd.js
@@ -1,6 +1,9 @@
 // Create a function called removeEnd that takes in an array and removes the
 // value at the end of the array. Return the new array.
-
+function removeEnd(arr) {
+  arr.pop();
+  return arr;
+}
 var removeEnd;
 
 // DO NOT DELETE
